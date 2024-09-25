0ba9e33fab419c053b8c93039c25ba4fec62c2ac
oleksii_ozyrskyi <aozyrskyii@gmail.com>
2024-09-25 11:42:11 +0300

    _v

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -28,6 +28,7 @@ export const getContactsController = async (req, res, next) => {
   });
 
   res.status(200).json({
+    status: 200,
     message: 'Successfully found contacts!',
     data: contacts,
   });
@@ -43,6 +44,7 @@ export const getContactByIdController = async (req, res, next) => {
   }
 
   res.status(200).json({
+    status: 200,
     message: `Successfully found contact with id ${contactId}!`,
     data: contact,
   });
@@ -68,9 +70,10 @@ export const createContactController = async (req, res, next) => {
     photo: photoUrl,
   };
 
-  const contact = await createContact(contactData);
+  const contact = await createContact(contactData).lean();
 
   res.status(201).json({
+    status: 201,
     message: 'Successfully created a contact!',
     data: contact,
   });
