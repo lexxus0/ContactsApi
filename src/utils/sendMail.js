86091d0e616a6b798a8c2b51f7c76603a598953a
oleksii_ozyrskyi <aozyrskyii@gmail.com>
2024-10-08 17:57:13 +0200

    changed app_domain

--- src/utils/sendMail.js
+++ src/utils/sendMail.js
@@ -6,12 +6,22 @@ import { env } from '../utils/env.js';
 const transporter = nodemailer.createTransport({
   host: env(SMTP.SMTP_HOST),
   port: Number(env(SMTP.SMTP_PORT)),
+  secure: false,
   auth: {
     user: env(SMTP.SMTP_USER),
     pass: env(SMTP.SMTP_PASSWORD),
   },
+  tls: {
+    rejectUnauthorized: false,
+  },
 });
 
 export const sendEmail = async (options) => {
-  return await transporter.sendMail(options);
+  try {
+    const info = await transporter.sendMail(options);
+    return info;
+  } catch (error) {
+    console.error('Error:', error.message);
+    throw error;
+  }
 };
