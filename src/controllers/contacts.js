07b78dc672c5da2dd2dd205b1c091e47739f473e
oleksii_ozyrskyi <aozyrskyii@gmail.com>
2024-10-13 15:09:51 +0200

    query params

--- src/controllers/contacts.js
+++ src/controllers/contacts.js
@@ -18,7 +18,7 @@ export const getContactsController = async (req, res, next) => {
   const { page, perPage } = parsePaginationParams(req.query);
   const { sortBy, sortOrder } = parseSortParams(req.query);
   const filter = { ...parseFilterParams(req.query), userId };
-
+  ('');
   const contacts = await getAllContacts({
     page,
     perPage,
